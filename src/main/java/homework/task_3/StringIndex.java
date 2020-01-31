8b42b8d9f85761ca14e8fd7112bc5a6290ac16e6
Maddushka <gmadbull@gmail.com>
2020-01-31 19:16:33 +0300

    add new class StringIndex

--- src/main/java/homework/task_3/StringIndex.java
+++ src/main/java/homework/task_3/StringIndex.java
@@ -16,7 +16,27 @@ public class StringIndex {
             outputString = outputString.concat(String.valueOf(indexword));
             System.out.println(outputString);
         }
-
+        spoofWord(text);
+        reverseWord(text);
     }
 
+    private void spoofWord(String text){
+        StringBuilder stringB = new StringBuilder(text);
+        Scanner sc = new Scanner(System.in);
+        String indexText = "Введиту два номера индекса для замены в строке: ";
+        System.out.println(indexText);
+        int index1 = sc.nextInt();
+        int index2 = sc.nextInt();
+        char symbol1= stringB.charAt(index1);
+        char symbol2 = stringB.charAt(index2);
+        stringB.setCharAt(index1,symbol2);
+        stringB.setCharAt(index2,symbol1);
+        System.out.println(stringB);
+    }
+
+    private void reverseWord(String text){
+        String rightPart;
+        String leftPart;
+        int lenght = text.length();
+    }
 }
