9aeee71c1093b8e8af7f31d0df5bb9de37cd29d0
Maddushka <gmadbull@gmail.com>
2020-02-02 00:31:11 +0300

    add new class StringIndex

--- src/main/java/homework/task_3/StringIndex.java
+++ src/main/java/homework/task_3/StringIndex.java
@@ -1,42 +1,74 @@
 package homework.task_3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StringIndex {
+    int index;
+    Scanner sc = new Scanner(System.in);
 
-    public void stringIndGen(String text){
+
+    public void stringIndGen(String text) {
         String indextext = "Введите индекс (введите 666, что бы выйти):";
         String outputString = new String();
-        Scanner sc = new Scanner(System.in);
+        char indexChar;
         while (true) {
             System.out.println(indextext);
-            int index = sc.nextInt();
+            index = sc.nextInt();
+            if (index > (text.length()-1))verifyIndex(text);
             if (index == 666) break;
-            char indexword = text.charAt(index);
-            outputString = outputString.concat(String.valueOf(indexword));
+            indexChar = text.charAt(index);
+            outputString = outputString.concat(String.valueOf(indexChar));
             System.out.println(outputString);
         }
-        spoofWord(text);
-        reverseWord(text);
+        System.out.println(spoofChar(text));
+        System.out.println(reverseString(text));
     }
 
-    private void spoofWord(String text){
-        StringBuilder stringB = new StringBuilder(text);
+    private void verifyIndex( String text) {
+        String tooMany = "Слишком большое число, попробуйте не больше ";
+        while (true) {
+            System.out.println(tooMany + (text.length()-1));
+            index = sc.nextInt();
+            if (index < (text.length())) break;
+        }
+    }
+
+    private String spoofChar(String text) {
+        StringBuilder stringForSpoof = new StringBuilder(text);
         Scanner sc = new Scanner(System.in);
         String indexText = "Введиту два номера индекса для замены в строке: ";
         System.out.println(indexText);
         int index1 = sc.nextInt();
         int index2 = sc.nextInt();
-        char symbol1= stringB.charAt(index1);
-        char symbol2 = stringB.charAt(index2);
-        stringB.setCharAt(index1,symbol2);
-        stringB.setCharAt(index2,symbol1);
-        System.out.println(stringB);
+        char symbol1 = stringForSpoof.charAt(index1);
+        char symbol2 = stringForSpoof.charAt(index2);
+        stringForSpoof.setCharAt(index1, symbol2);
+        stringForSpoof.setCharAt(index2, symbol1);
+        String result = stringForSpoof.toString();
+        return result;
     }
 
-    private void reverseWord(String text){
-        String rightPart;
-        String leftPart;
-        int lenght = text.length();
+    private String reverseString(String text) {
+        ArrayList<String> arrWords = new ArrayList<String>(Arrays
+                .asList(text.split(" ")));
+        StringBuilder reverse;
+        String result = new String();
+        String space = " ";
+        for (int i = 0; i < arrWords.size(); i++) {
+            reverse = new StringBuilder(reverseWord(arrWords.get(i)));
+            arrWords.set(i, reverse.toString());
+            result = result.concat(arrWords.get(i));
+            result = result.concat(space);
+        }
+
+        return result;
     }
+
+    private StringBuilder reverseWord(String word) {
+        StringBuilder sb = new StringBuilder(word).reverse();
+        return sb;
+    }
+
 }
