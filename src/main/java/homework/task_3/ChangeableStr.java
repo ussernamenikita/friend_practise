97a95ccdfe78a3b51b40b6b99d5c1b064964121a
Maddushka <gmadbull@gmail.com>
2020-02-10 18:15:52 +0300

    Final refactor cod by task 3 add todo 1 by task 3

--- src/main/java/homework/task_3/ChangeableStr.java
+++ src/main/java/homework/task_3/ChangeableStr.java
@@ -7,19 +7,22 @@ import java.util.Scanner;
 
 public class ChangeableStr {
     Scanner sc = new Scanner(System.in);
+    private final String ERR_GET_INDEX = "Ошибка! Попробуйте арабские цифры...";
 
 
-    public void stringIndGen() {
+    public void stringIndexGeneration() {
+        ProcessedString ps = new ProcessedString();
+        String textGetIndex = "Введите индекс (введите 666, что бы выйти):";
         String modifiedStr = "";
         char charByIndex;
-        ProcessedString ps = new ProcessedString();
         String str = ps.received;
         int index = 0;
 
+        System.out.println(textGetIndex);
         while (true) {
-            index = checkIndex(index);
+            index = checkIndexForNumber(index);//+
             if (index == 666) break;
-            if (index > (str.length())) index = verifyLengthIndex(str, index);
+            index = verifyLengthIndex(str, index);
             charByIndex = str.charAt(index);
             modifiedStr = modifiedStr.concat(String.valueOf(charByIndex));
             System.out.println(modifiedStr);
@@ -29,17 +32,13 @@ public class ChangeableStr {
         System.out.println(reverseString(str));
     }
 
-    private int checkIndex(int index) {
-        String textGetIndex = "Введите индекс (введите 666, что бы выйти):";
-        String errGetIndex = "Ошибка! Попробуйте арабские цифры...";
-
-        System.out.println(textGetIndex);
+    private int checkIndexForNumber(int index) {
         while (true) {
             try {
                 index = sc.nextInt();
                 break;
             } catch (InputMismatchException e) {
-                System.out.println(errGetIndex);
+                System.out.println(ERR_GET_INDEX);
                 sc.next();
             }
         }
@@ -48,37 +47,56 @@ public class ChangeableStr {
 
     private int verifyLengthIndex(String str, int index) {
         String tooMany = "Слишком большое число, попробуйте не больше ";
-        while (true) {
-            System.out.println(tooMany + (str.length() - 1));
-            index = sc.nextInt();
-            if (index == 666) break;
-            if (index < (str.length())) break;
+        if (index > (str.length()) - 1) {
+            while (true) {
+                System.out.println(tooMany + (str.length() - 1));
+                index = checkIndexForNumber(index);
+                if (index == 666) break;
+                if (index < (str.length())) break;
+            }
         }
         return index;
     }
 
-    //todo 1 обработать ошибку (String index out of range)
-    private String spoofChar(String text) {
-        StringBuilder stringForSpoof = new StringBuilder(text);
-        Scanner sc = new Scanner(System.in);
-        String indexText = "Введиту два номера индекса для замены в строке: ";
-        System.out.println(indexText);
-        int index1 = sc.nextInt();
-        int index2 = sc.nextInt();
+    private String spoofChar(String strForSpoof) {
+        StringBuilder stringForSpoof = new StringBuilder(strForSpoof);
+        int index1 = verifyLengthIndex(strForSpoof, getIndexForSpoof(strForSpoof));
+        int index2 = verifyLengthIndex(strForSpoof, getIndexForSpoof(strForSpoof));
         char symbol1 = stringForSpoof.charAt(index1);
         char symbol2 = stringForSpoof.charAt(index2);
+
         stringForSpoof.setCharAt(index1, symbol2);
         stringForSpoof.setCharAt(index2, symbol1);
+
         String result = stringForSpoof.toString();
+
         return result;
     }
 
-    private String reverseString(String text) {
+    private int getIndexForSpoof(String str) {
+        String indexText = "Введите номер индекса для замены в строке: ";
+        System.out.println(indexText);
+        int index;
+
+        while (true) {
+            try {
+                index = sc.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println(ERR_GET_INDEX);
+                sc.next();
+            }
+        }
+        return index;
+    }
+
+    private String reverseString(String stringForReverse) {
         ArrayList<String> arrWords = new ArrayList<String>(Arrays
-                .asList(text.split(" ")));
+                .asList(stringForReverse.split(" ")));
         StringBuilder reverse;
         String result = new String();
         String space = " ";
+
         for (int i = 0; i < arrWords.size(); i++) {
             reverse = new StringBuilder(reverseWord(arrWords.get(i)));
             arrWords.set(i, reverse.toString());
--- src/main/java/homework/task_3/Main.java
+++ src/main/java/homework/task_3/Main.java
@@ -2,13 +2,14 @@ package homework.task_3;
 
 
 public class Main {
+//todo 1  create doc by all methods of task 3
 
     public static void main(String[] args) {
         ProcessedString ps = new ProcessedString();
         ChangeableStr si = new ChangeableStr();
 
         ps.getString();
-        si.stringIndGen();
+        si.stringIndexGeneration();
     }
 
 }
